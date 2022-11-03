527a19a6427a485e5d9da348fe48ee94baaf4ca0
thanatos-vf-2000 <32266803+thanatos-vf-2000@users.noreply.github.com>
2022-11-03 22:45:04 +0100

    v1.3.5
    * Correction summary dashbord (exclude local location),
    * Update psr/cache v1.0.1 => v3.0.0,
    * Update GeoIP Database 20221104.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit5cc0af48f7c4a39876f9e2e63c332369
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5cc0af48f7c4a39876f9e2e63c332369', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit5cc0af48f7c4a39876f9e2e63c332369', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit5cc0af48f7c4a39876f9e2e63c332369::getInitializer($loader));
         } else {
